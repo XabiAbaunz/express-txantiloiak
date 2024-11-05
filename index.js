d07eb85d482bd0518ea966b950fc5a307650cf3d
Juanan Pereira <juanan.pereira@ehu.eus>
2024-11-05 21:35:46 +0100

    update server configuration and clean up response messages

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
-const PORT = 4000
+const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
     res.render('index', {
@@ -22,27 +22,25 @@ app.get('/', (req, res) => {
 
 app.get('/testquery', (req, res) => {
     res.send("Kaixo GET " + JSON.stringify(req.query))
+    if (req.query.izena == "Ane") {
+        console.log("Kaixo Ane")
+    }
 })
 
 app.get("/test", function(req, res) {
     console.log("Hau test bat da")
-    // this is wrong, somEthing is missing...
+    // this is wrong, something is missing...
 })
 
 
 app.get("/user/:zer/:identifikatzaile", function(req, res) {
-    res.send(`Zer: ${req.params.zer}  ${req.params.identifikatzaile} !`)
+    res.send(`Zer: ${req.params.zer}  ${req.params.identifikatzaile} `)
 })
 
 
 app.post('/jaso', (req, res) => {
-    res.send(`Kaixo ${req.body.izen} ${req.body.abizen} ${req.query.test}}`)
+    res.send(`Kaixo ${req.body.izen} ${req.body.abizen} `)
 })
-
-app.put('/', (req, res) => {
-    res.send("Kaixo PUT " + JSON.stringify(req.query))
-})
-
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`)
